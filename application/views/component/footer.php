8c95cd04d9bf9714e3e8777763c046036815a499
fajaar121 <fajartrysutrasno@gmail.com>
2023-06-27 08:47:45 +0700

    Pagination

--- application/views/component/footer.php
+++ application/views/component/footer.php
@@ -4,15 +4,16 @@
   </div>
   <strong>Copyright &copy; <?= date('Y') ?></strong>
 </footer>
+</div>
 
 <!-- jQuery 3 -->
+<script src="<?php echo base_url() ?>assets/web_admin/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="<?php echo base_url() ?>assets/sweetalert/dist/sweetalert.min.js"></script>
 <!-- DataTables -->
 <script src="<?php echo base_url() ?>assets/web_admin/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="<?php echo base_url() ?>assets/web_admin/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <!-- SlimScroll -->
 <script src="<?php echo base_url() ?>assets/web_admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="<?php echo base_url() ?>assets/web_admin/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="<?php echo base_url() ?>assets/web_admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- FastClick -->
@@ -23,33 +24,26 @@
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url() ?>assets/web_admin/dist/js/demo.js"></script>
 
+<!-- page script -->
 <script>
   jQuery(document).ready(function($){
-    $('.btn-delete').on('click',function(){
-      var getLink = $(this).attr('href');
-        swal({
-          title: 'Delete Data',
-          text: 'Yakin Ingin Menghapus Data ?',
-          html: true,
-          confirmButtonColor: '#d9534f',
-          showCancelButton: true,
-        },function(){
-          window.location.href = getLink
-        });
-        return false;
-    });
+      $('.btn-delete').on('click',function(){
+          var getLink = $(this).attr('href');
+          swal({
+                  title: 'Delete Data',
+                  text: 'Yakin Ingin Menghapus Data ?',
+                  html: true,
+                  confirmButtonColor: '#d9534f',
+                  showCancelButton: true,
+                  },function(){
+                  window.location.href = getLink
+              });
+          return false;
+      });
   });
 
   $(function () {
-    $('#example1').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : false,
-      'ordering'    : false,
-      'info'        : true,
-      'autoWidth'   : false
-
-    })
+    $('#example1').DataTable();
     $('#example2').DataTable({
       'paging'      : true,
       'lengthChange': false,
@@ -61,16 +55,6 @@
   });
 </script>
 
-<script type="text/javascript">
-  $(".form_datetime").datetimepicker({
-    format: 'dd/mm/yyyy',
-    autoclose: true,
-    todayBtn: true,
-    pickTime: false,
-    minView: 2,
-    maxView: 4,
-  });
-</script>
 </body>
 
 </html>
